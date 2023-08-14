45731c4c97062b38b60eecaef26e5e8f59bcf67b
Shweta Mishra <shweta.aith@gmail.com>
2023-08-14 21:22:59 +0530

    hemat

--- hemat/src/component/Home.jsx
+++ hemat/src/component/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
     return <div className={styled.main_div}> 
 
         <div className={styled.infomation_div}>
-
 <div className={styled.inner}>
 
     <div className={styled.mid_aline}>
